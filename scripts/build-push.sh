123fce4dfd91beac8f2f2dc17190feffba84419f
minerun <sa7020x@gmail.com>
2022-04-25 19:04:42 +0900

    test build

--- scripts/build-push.sh
+++ scripts/build-push.sh
@@ -5,11 +5,11 @@ set -e
 use_tag="duck27/nginx-http-flv:$NAME"
 use_dated_tag="${use_tag}-$(date -I)"
 
+bash scripts/docker-login.sh
+
 bash scripts/build.sh
 
 docker tag "$use_tag" "$use_dated_tag"
-
-bash scripts/docker-login.sh
 
 docker push "$use_tag"
 docker push "$use_dated_tag"
